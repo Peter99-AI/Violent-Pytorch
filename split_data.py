a92e4d610fe940e4c2cf7195e238919671ee31fe
Peter99-AI <peter.cta99@gmail.com>
2021-09-03 17:20:46 +0700

    Done

---
+++ split_data.py
@@ -0,0 +1,40 @@
+import os
+import numpy as np
+import shutil
+import os.path
+
+img_file = r"img"
+root_dir = r"data"
+valid_ratio = 0.2
+
+if not os.path.exists(root_dir + '/train'):
+    os.makedirs(root_dir + '/train')
+if not os.path.exists(root_dir + '/valid'):
+    os.makedirs(root_dir + '/valid')
+
+all_filename = os.listdir(img_file)
+np.random.shuffle(all_filename)
+train_files, valid_files = np.split(np.array(all_filename), [int(len(all_filename) * (1 - valid_ratio))])
+
+# train_files  = [src + "/train/" + name for name in train_files.tolist()]
+# valid_files  = [src + "/valid/" + name for name in valid_files.tolist()]
+
+print("train-list : ", train_files)
+print('valid-list : ', valid_files)
+
+print("*****************************")
+print('Total images: ', len(all_filename))
+print('Training: ', len(train_files))
+print('Testing: ', len(valid_files))
+print("*****************************")
+
+
+for name in train_files:
+    src = img_file +"/"+ name
+    dst = root_dir + '/train/nofight/' + name
+    os.rename(src,dst)
+
+for name in valid_files:
+    src = img_file +"/"+ name
+    dst = root_dir + '/valid/nofight/' + name
+    os.rename(src,dst)
