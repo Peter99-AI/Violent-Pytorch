643ce26679a66dbeeb5ca1213a30a9f19a8e5b9c
Peter99-AI <peter.cta99@gmail.com>
2021-09-07 16:03:53 +0700

    Done

--- split_data.py
+++ split_data.py
@@ -16,11 +16,17 @@ all_filename = os.listdir(img_file)
 np.random.shuffle(all_filename)
 train_files, valid_files = np.split(np.array(all_filename), [int(len(all_filename) * (1 - valid_ratio))])
 
+n1 = len(train_files) % 15
+train_files = train_files[n1:]
+m1 = len(valid_files) % 15
+valid_files = valid_files[m1:]
+
 print("*****************************")
 print('Total images: ', len(all_filename))
 print('Training: ', len(train_files))
 print('Testing: ', len(valid_files))
 print("*****************************")
+
 
 
 for name in train_files:
